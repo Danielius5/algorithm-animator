82183dfbbda06a8a4765d51bfc1e53b727a839ff
Danielius Sukys <sukys.danielius@gmail.com>
2023-09-21 16:03:45 +0100

    Experiment with React Flow

---
+++ src/experiments/react-flow-experiment/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactFlow from 'reactflow';
+import { SmartBezierEdge } from '@tisoap/react-flow-smart-edge'
+
+import 'reactflow/dist/style.css';
+
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2', sourcePosition:"left", tagetPosition:"b" }];
+
+const edgeTypes = {
+	smart: SmartBezierEdge
+}
+
+export default function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow nodes={initialNodes} edges={initialEdges} edgeTypes={edgeTypes} />
+    </div>
+  );
+}
--- timelog.md
+++ timelog.md
@@ -23,3 +23,4 @@
 * *1.0 hour* Read Algorithmics 1 slides and other online materials about DFA's and redo exercises from Algorithmics 1
 * *3.0 hours* Experimented building a static DFA using Cytoscape JS.
 * *1.0 hours* Experimented building a static DFA using Sigma.js
+* *0.5 hours* Experimented building a static DFA using React Flow
