2fdf5c30bfbad04d32c6aafcb2d54629686ac33d
Danielius Sukys <sukys.danielius@gmail.com>
2024-01-25 15:06:10 +0000

    Fix active transition with multiple chars

--- src/algorithm-animator/src/components/GraphFromDFA.tsx
+++ src/algorithm-animator/src/components/GraphFromDFA.tsx
@@ -53,7 +53,7 @@ function recursiveAppendGraph(graph: string[], state: State, visited: Set<string
       }
       transitionsToCharacters[key].push(transition.characterMatched)
       isTransitionAdded[key] = false
-      isTransitionActive[key] = transition.active ?? false
+      isTransitionActive[key] = isTransitionActive[key] || (transition.active ?? false)
     }
     recursiveAppendGraph(graph, transition.stateTo, visited, selectedStates);
   }
