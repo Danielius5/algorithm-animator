c4d7104967979c22869978b99e0704b7abe24c45
Danielius Sukys <sukys.danielius@gmail.com>
2024-01-31 14:27:32 +0000

    Fix bugs in animation, other enhancements

--- src/algorithm-animator/src/components/GraphAnimator.tsx
+++ src/algorithm-animator/src/components/GraphAnimator.tsx
@@ -89,8 +89,8 @@ export function GraphAnimator({states, text, setCurrentLetter, setCurrentState, 
     return (
         <div>
             <GraphFromDFA id={id ||"g-animator"} states={states} />
-            <input type="button" onClick={() => {setCurrentStep(Math.min(currentStep + 1, steps.length - 1)); setGoForward(true)}}  value="Next" id="animate-button-next" />
-            <input type="button" onClick={() => {setCurrentStep(Math.max(currentStep - 1, 0)); setGoForward(false)}}  value="Back" id="animate-button-back" />
+            <input disabled={!text || text.length === 0} type="button" onClick={() => {setCurrentStep(Math.min(currentStep + 1, steps.length - 1)); setGoForward(true)}}  value="Next" id="animate-button-next" />
+            <input disabled={!text || text.length === 0} type="button" onClick={() => {setCurrentStep(Math.max(currentStep - 1, 0)); setGoForward(false)}}  value="Back" id="animate-button-back" />
 
         </div>
     )
